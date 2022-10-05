d8ccb2c058c7c35e54f894dbbe3bfd71f4e7b911
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-05 18:34:41 +0300

    Add mission reducer

--- src/redux/Mission/mission.js
+++ src/redux/Mission/mission.js
@@ -1,25 +1,54 @@
-// const MISSION_URL = 'https://api.spacexdata.com/v3/missions';
-const GET_MISSION = 'GET_MISSION';
-// const JOIN_MISSION = 'JOIN_MISSION';
-// const LEAVE_MISSION = 'LEAVE_MISSION';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const getMissions = (missions) => {
-    return {
-        type: GET_MISSION,
-        payload: missions,
-    }
-}
+export const fetchMission = createAsyncThunk(
+    "missions/fetch/missions",
+    async () => {
+        const missions = await fetch("https://api.spacexdata.com/v3/missions");
+        console.log(missions)
+        const data = await missions.json();
+        console.log(data)
+        return data;
+    },
 
-const initialState = [];
+);
 
+const missionSlice = createSlice({
+    name: 'missions',
+    initialState: {
+        missions: [],
+        status: 'idle',
+        error: null,
+    },
+    reducers: {
+        JoinMission(state, action) {
+            const id = action.payload;
+            const ExistingMission = state.missions.find((mission) => mission.id === id);
+            if (ExistingMission) {
+                ExistingMission.joined = true;
+            }
+        },
+        LeaveMission(state, action) {
+            const id = action.payload;
+            const ExistingMission = state.missions.find((mission) => mission.id === id);
+            if (ExistingMission) {
+                ExistingMission.joined = false;
+            }
+        },
+    },
+    extraReducers: {
+        [fetchMission.pending]: (state) => {
+            state.status = 'loading';
+        },
+        [fetchMission.fulfilled]: (state, action) => {
+            state.status = 'success';
+            state.missions = [...action.payload];
+        },
+        [fetchMission.error]: (state, action) => {
+            state.status = 'Error';
+            state.error = action.error.message;
+        },
+    },
+});
 
-const missionReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_MISSION:
-            return { ...action.payload };
-        default:
-            return state;
-    }
-};
-
-export default missionReducer;
+export default missionSlice.reducer;
+export const { JoinMission, LeaveMission } = missionSlice.actions;
