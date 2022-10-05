8df2702bd5eeed54cf638e8f9d2a0ab6a4b303f9
kidde60 <kgeorgewilliam60@gmail.com>
2022-10-05 15:40:07 +0300

    add changes

--- src/components/missions/missionCard.js
+++ src/components/missions/missionCard.js
@@ -1,54 +1,61 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import React from "react";
+import { useSelector } from "react-redux";
 
-const MissionCard = ({ description, name, status }) => (
-    <>
-        <div>
-            <p>{name}</p>
-        </div>
-        <p>{description}</p>
-        <div>
-            {status
-                ? (
-                    <p
-                        style={{ minWidth: '120px' }}
-                    >
-                        ACTIVE MEMBER
-                    </p>
+const MissionCard = () => {
+    const Missions = useSelector((state) => state.missions)
+    // const { name, description, status } = missions
+    return (
+        <>
+            <div>
+                {/* <p>{name}</p> */}
+            </div>
+            {/* <p>{description}</p> */}
+            <div>
+                {/* {status */}
+                {/* ? (
+                        <p
+                            style={{ minWidth: '120px' }}
+                        >
+                            ACTIVE MEMBER
+                        </p>
+                    ) : (
+                        <p
+                            style={{ minWidth: '120px' }}
+                        >
+                            NOT A MEMBER
+                        </p>
+                    )} */}
+            </div>
+            <div>
+                {/* {!status */}
+                {/* ? (
+                <button
+                    // onClick={() => dispatch(joiningMissionAction(mission.id))}
+                    type="button"
+                    style={{ minWidth: '100px' }}
+                >
+                    Join Mission
+                </button>
                 ) : (
-                    <p
-                        style={{ minWidth: '120px' }}
-                    >
-                        NOT A MEMBER
-                    </p>
-                )}
-        </div>
-        <div>
-            {!status
-                ? (
-                    <button
-                        // onClick={() => dispatch(joiningMissionAction(mission.id))}
-                        type="button"
-                        style={{ minWidth: '100px' }}
-                    >
-                        Join Mission
-                    </button>
-                ) : (
-                    <button
-                        // onClick={() => dispatch(leavingMissionAction(mission.id))}
-                        type="button"
-                        style={{ minWidth: '100px' }}
-                    >
-                        Leave Mission
-                    </button>
-                )}
-        </div>
-    </>
-);
+                <button
+                    // onClick={() => dispatch(leavingMissionAction(mission.id))}
+                    type="button"
+                    style={{ minWidth: '100px' }}
+                >
+                    Leave Mission
+                </button>
+                    )} */}
+            </div>
+        </>
+    )
+};
 
 export default MissionCard;
 
-MissionCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    status: PropTypes.bool.isRequired,
-};
+// MissionCard.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     description: PropTypes.string.isRequired,
+//     status: PropTypes.bool.isRequired,
+// };
+
--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -1,20 +1,42 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 import MissionCard from './missionCard';
+import { getMissions } from '../../redux/Mission/mission'
 
 const Mission = () => {
-  const missions = useSelector((state) => state.missions);
-  console.log(missions);
-  const { name, description, status } = missions
-  return (
-    <MissionCard
-      name={name}
-      description={description}
-      status={status}
-    />
+  const Missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch
 
-
+  const fetchMisssions = createAsyncThunk(
+    "missions/fetch/missions",
+    async () => {
+      const missions = await fetch("https://api.spacexdata.com/v3/rockets");
+      const data = await missions.json();
+      console.log(data)
+      return data;
+    },
   );
+  fetchMisssions()
+
+  // const missionUrl = 'https://api.spacexdata.com/v3/missions'
+  // const fetchMission = async () => {
+  //   const response = await axios.get(missionUrl).catch((error) => {
+  //     console.log('error', error)
+  //   })
+  //   console.log(response.data)
+  //   dispatch(getMissions(response.data))
+
+  // }
+  // useEffect(() => {
+  //   fetchMission();
+  // }, []);
+  // console.log(Missions);
+  // return (
+  //   <MissionCard />
+
+  // );
 };
 
 export default Mission;
